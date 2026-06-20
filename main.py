39f018ff24a3c2e029ab3d37196bd18469695e55
katGhost <katghost124@gmail.com>
2026-06-20 18:57:13 +0200

    read xml, seummarize content intent -> task, save response in .txt file

--- main.py
+++ main.py
@@ -1,5 +1,52 @@
+from ollama import chat
+import os
+
+# read from file, read content
+def read_content():
+    path = os.path.join('xml', 'reminder.xml')
+    
+    with open(path, 'r', encoding="utf-8") as f:
+        return f.read()
+
+def summarize(xml_content):
+    """Model responses -> prompt engineering happens below"""
+    response = chat(
+        model='llama3.2',
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a personal assistant."
+            },
+            {
+                "role": "user",
+                "content": xml_content
+            }
+        ],
+        # think='medium',
+        stream=False,
+        options={
+            "temperature": 0.7,
+            "num_predict": 512,
+        },
+    )
+    
+    return response.message.content
+
+def save_response(content):
+    """write to file in files/, save the response from the llm"""
+    os.makedirs('files/', exist_ok=True)
+    
+    output_path = os.path.join('files', 'reminder.txt')
+    
+    with open(output_path, 'w', encoding="utf-8") as f:
+        f.write(content)
+
 def main():
-    print("Hello from ai!")
+    xml_content = read_content()
+    
+    response_text = summarize(xml_content)
+    
+    save_response(response_text)
 
 
 if __name__ == "__main__":
